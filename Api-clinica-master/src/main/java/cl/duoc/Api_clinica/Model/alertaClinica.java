8017e3db7dd7b3067fae6773fe41a1ba4b140086
simonsalve25 <si.monsalve@duocuc.cl>
2026-06-29 00:07:37 -0400

    creación alerta clinica

---
+++ Api-clinica-master/src/main/java/cl/duoc/Api_clinica/Model/alertaClinica.java
@@ -0,0 +1,32 @@
+import jakarta.persistence.*; 
+import jakarta.validation.Valid; 
+import jakarta.validation.constraints.NotBlank; 
+import jakarta.validation.constraints.Pattern; 
+import lombok.Data;
+
+@Entity
+@Table(name = "alertas_clinicas")
+@Data
+public class AlertaClinica {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Ingrese la enfermedad.")
+    private String enfermedadesPreexistentes;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Ingrese las alergias.")
+    private String alergias;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Ingrese los medicamentos.")
+    private String medicamentos;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Ingrese observaciones.")
+    private String observaciones;
+
+}
