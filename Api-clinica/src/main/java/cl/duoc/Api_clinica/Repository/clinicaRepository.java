c84f0c9b8f9a0b5d7c0e8db07793c7bdf776efb7
simonsalve25 <si.monsalve@duocuc.cl>
2026-06-29 01:57:00 -0400

    Delete Api-clinica directory

--- Api-clinica/src/main/java/cl/duoc/Api_clinica/Repository/clinicaRepository.java
+++
@@ -1,7 +0,0 @@
-package cl.duoc.Api_clinica.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import cl.duoc.Api_clinica.Model.clinicaModel;
-
-public interface clinicaRepository extends JpaRepository<clinicaModel, Long> {
-}
